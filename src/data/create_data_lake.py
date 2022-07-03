d3103ddb1a2cc437792a5ad7fc378bd0add829cb
Diego Mayorga <dcmayorgab@unal.edu.co>
2022-07-03 12:21:59 -0500

    crear datalake

--- src/data/create_data_lake.py
+++ src/data/create_data_lake.py
@@ -24,17 +24,19 @@ def create_data_lake():
 
     """
     # raise NotImplementedError("Implementar esta función")
-    os.mkdir('data_lake')
-    os.mkdir('data_lake/landing')
-    os.mkdir('data_lake/raw')
-    os.mkdir('data_lake/cleansed')
-    os.mkdir('data_lake/business')
-    os.mkdir('data_lake/business/reports')
-    os.mkdir('data_lake/business/reports/figures')
-    os.mkdir('data_lake/business/features')
-    os.mkdir('data_lake/business/forecasts')
+    cwd = os.getcwd()
+    os.mkdir(os.path.join(cwd, 'data_lake'))
+    os.mkdir(os.path.join(cwd,'data_lake/landing'))
+    os.mkdir(os.path.join(cwd,'data_lake/raw'))
+    os.mkdir(os.path.join(cwd,'data_lake/cleansed'))
+    os.mkdir(os.path.join(cwd,'data_lake/business'))
+    os.mkdir(os.path.join(cwd,'data_lake/business/reports'))
+    os.mkdir(os.path.join(cwd,'data_lake/business/reports/figures'))
+    os.mkdir(os.path.join(cwd,'data_lake/business/features'))
+    os.mkdir(os.path.join(cwd,'data_lake/business/forecasts'))
     
+
 if __name__ == "__main__":
     import doctest
-
+    create_data_lake() 
     doctest.testmod()

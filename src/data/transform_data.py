a63ac32692e6345746f6f9cd0f3ecdea455fbd67
Diego Mayorga <dcmayorgab@unal.edu.co>
2022-07-03 20:44:21 -0500

    transformar data

--- src/data/transform_data.py
+++ src/data/transform_data.py
@@ -1,3 +1,7 @@
+
+import pandas as pd
+
+
 def transform_data():
     """Transforme los archivos xls a csv.
 
@@ -7,10 +11,39 @@ def transform_data():
     H23.
 
     """
-    raise NotImplementedError("Implementar esta función")
+    #raise NotImplementedError("Implementar esta función")
 
+    cwd=os.getcwd()
+    path_landing = os.path.join(cwd, 'data_lake/landing/') 
+    path_raw = os.path.join(cwd, 'data_lake/raw/') 
+    list_files = os.listdir(path_landing)
+
+    for file in list_files:
+        
+        
+        try :
+            df_energia = pd.read_excel(path_landing + file,engine='openpyxl')
+    
+            
+        except :
+            df_energia = pd.read_excel(path_landing + file )
+        
+        if  df_energia.columns[0]!='Fecha':
+            skip=df_energia.iloc[:,0][df_energia.iloc[:,0]=='Fecha'].index.tolist()[0]
+            df_energia=df_energia.iloc[skip:,:]
+            new_header = df_energia.iloc[0]
+            df_energia = df_energia[1:] 
+            df_energia.columns = new_header
+        
+        df_energia=df_energia.iloc[:,:25]
+        #df_energia=df_energia[[i for i  in df_energia.columns if i not in ['Version']]
+        df_energia = df_energia.melt(id_vars=['Fecha'], value_name='Precio', var_name='Hora')
+        df_energia['Hora']=df_energia['Hora'].astype(int)
+        df_energia['Fecha']=pd.to_datetime(df_energia['Fecha'], format='%Y-%m-%d')
+        df_energia.to_csv(os.path.join(path_raw,file.split('.')[0]+'.csv'),index=False)
 
 if __name__ == "__main__":
     import doctest
+    transform_data()
 
     doctest.testmod()

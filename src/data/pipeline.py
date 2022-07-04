ae6ce9751f16d98f958cbbf04fe881a4b14555ba
Diego Mayorga <dcmayorgab@unal.edu.co>
2022-07-04 03:09:20 -0500

    pipeline test1

--- src/data/pipeline.py
+++ src/data/pipeline.py
@@ -11,13 +11,77 @@ En luigi llame las funciones que ya creo.
 
 
 """
+import ingest_data
+import transform_data
+import clean_data
+import compute_daily_prices
+import compute_monthly_prices
+import luigi
 
+class IngestData(luigi.Task):
 
-if __name__ == "__main__":
+    def output(self):
+        return luigi.LocalTarget('ingest.txt')
 
-    raise NotImplementedError("Implementar esta función")
+    def run(self):
+        with self.output().open('w') as f:
+            ingest_data.ingest_data()
+        
+        
+class TransformData(luigi.Task):
+    
+    def requires(self):
+        return IngestData()
+    
+    def output(self):
+        return luigi.LocalTarget("TransformData.txt")
 
+    def run(self):
+        with self.output().open("w") as outfile:
+            transform_data.transform_data()   
+
+class cleanData(luigi.Task):
+    def requires(self):
+        return TransformData()
+
+    def output(self):
+        return luigi.LocalTarget("cleanData.txt")
+
+    def run(self):
+        with self.output().open("w") as outfile:
+            clean_data.clean_data()
+
+class dailyReports(luigi.Task):
+    def requires(self):
+        return cleanData()
+
+    def output(self):
+        return luigi.LocalTarget("dailyReports.txt")
+
+    def run(self):
+        with self.output().open("w") as outfile:
+            compute_daily_prices.compute_daily_prices()
+            
+class monthlyReports(luigi.Task):
+    def requires(self):
+        return cleanData()
+
+    def output(self):
+        return luigi.LocalTarget("monthlyReports.txt")
+
+    def run(self):
+        with self.output().open("w") as outfile:
+            compute_monthly_prices.compute_monthly_prices()
+
+class reports_prices(luigi.Task):
+    def requires(self):
+        return [dailyReports(), monthlyReports()]
+    
 if __name__ == "__main__":
     import doctest
 
+    luigi.run(["reports_prices"])
     doctest.testmod()
+
+
+

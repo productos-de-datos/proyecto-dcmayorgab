1f4a23f1e7d6ff8e4781d8f0605f67f2868ed367
Diego Mayorga <dcmayorgab@unal.edu.co>
2022-07-04 00:30:44 -0500

    limpieza de datos

--- src/data/clean_data.py
+++ src/data/clean_data.py
@@ -1,3 +1,6 @@
+import pandas as pd
+import os
+
 def clean_data():
     """Realice la limpieza y transformación de los archivos CSV.
 
@@ -12,10 +15,26 @@ def clean_data():
 
 
     """
-    raise NotImplementedError("Implementar esta función")
+    #raise NotImplementedError("Implementar esta función")
+
+    cwd=os.getcwd()
+    path_raw = os.path.join(cwd, 'data_lake/raw/') 
+    path_cleaned=os.path.join(cwd, 'data_lake/cleansed/') 
+    list_files = [os.path.join(cwd, 'data_lake/raw/'+i)  for i in os.listdir(path_raw)]
+
+    lst_df = []
+       
+    for path in list_files:
+
+        df = pd.read_csv(path)
+        lst_df.append(df)
+            
+        df = pd.concat(lst_df)
+        df.to_csv(os.path.join(path_cleaned,'precios-horarios.csv'),index=False)
+
 
 
 if __name__ == "__main__":
     import doctest
-
+    clean_data()
     doctest.testmod()

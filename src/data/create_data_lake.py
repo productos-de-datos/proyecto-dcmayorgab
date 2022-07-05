61f4e926c0c19a9e52148bc0d4696dd3089d4b3b
Diego Mayorga <dcmayorgab@unal.edu.co>
2022-07-05 09:38:49 -0500

    Documentacion

--- src/data/create_data_lake.py
+++ src/data/create_data_lake.py
@@ -4,20 +4,22 @@ Esta función debe crear la carpeta `data_lake` en la raiz del proyecto.
 """
 import os
 
-def create_data_lake():
 
+def create_data_lake():
     """
     Crea el data lake con sus capas.
     """
 
     os.mkdir("./data_lake")
-    carpetas = ["landing", "raw", "cleansed","business"]
+    carpetas = ["landing", "raw", "cleansed", "business"]
     carpetas_business = ["reports", "features", "forecasts"]
     [os.mkdir(os.path.join("data_lake/", c)) for c in carpetas]
-    [os.mkdir(os.path.join("data_lake/business/", c)) for c in carpetas_business]
+    [os.mkdir(os.path.join("data_lake/business/", c))
+     for c in carpetas_business]
     os.mkdir("./data_lake/business/reports/figures")
     return None
     #raise NotImplementedError("Implementar esta función")
+
 
 if __name__ == "__main__":
     import doctest

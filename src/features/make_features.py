562c38cf873387817c00cbe39b0df0cbfb362033
Diego Mayorga <dcmayorgab@unal.edu.co>
2022-07-05 04:25:35 -0500

    docstring

--- src/features/make_features.py
+++ src/features/make_features.py
@@ -1,3 +1,19 @@
+"""
+Cree el archivo data_lake/business/features/precios-diarios.csv. Este
+archivo contiene la información para pronosticar los precios diarios de la
+electricidad con base en los precios de los días pasados. Las columnas
+correspoden a las variables explicativas del modelo, y debe incluir,
+adicionalmente, la fecha del precio que se desea pronosticar y el precio
+que se desea pronosticar (variable dependiente).
+En la carpeta notebooks/ cree los notebooks de jupyter necesarios para
+analizar y determinar las variables explicativas del modelo.
+"""
+
+
+
+
+
+
 import os
 import pandas as pd
 
@@ -5,15 +21,7 @@ import pandas as pd
 def make_features():
     """Prepara datos para pronóstico.
 
-    Cree el archivo data_lake/business/features/precios-diarios.csv. Este
-    archivo contiene la información para pronosticar los precios diarios de la
-    electricidad con base en los precios de los días pasados. Las columnas
-    correspoden a las variables explicativas del modelo, y debe incluir,
-    adicionalmente, la fecha del precio que se desea pronosticar y el precio
-    que se desea pronosticar (variable dependiente).
-
-    En la carpeta notebooks/ cree los notebooks de jupyter necesarios para
-    analizar y determinar las variables explicativas del modelo.
+    Debido a que es una serie de tiempo no es necesario crear mas caracteristicas
 
     """
     #raise NotImplementedError("Implementar esta función")

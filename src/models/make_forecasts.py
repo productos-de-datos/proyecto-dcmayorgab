ceb9fcce17c1c5984104615ac3026a9e79e44745
Diego Mayorga <dcmayorgab@unal.edu.co>
2022-07-05 08:23:44 -0500

    except

--- src/models/make_forecasts.py
+++ src/models/make_forecasts.py
@@ -23,45 +23,49 @@ def make_forecasts():
     """
     # raise NotImplementedError("Implementar esta función")
     #cwd = os.getcwd()
-    path = "data_lake/business/features/precios-diarios.csv"
-    path_model = "models/precios-diarios.pkl"
-    path_forecasts = "data_lake/business/forecasts/precios-diarios.csv"
-    df = pd.read_csv(path)
-    data=list(df['Precio'])
-    data_d1 = [data[t] - data[t - 1] for t in range(1, len(data))]
+    try:
+        path = "data_lake/business/features/precios-diarios.csv"
+        path_model = "models/precios-diarios.pkl"
+        path_forecasts = "data_lake/business/forecasts/precios-diarios.csv"
+        df = pd.read_csv(path)
+        data=list(df['Precio'])
+        data_d1 = [data[t] - data[t - 1] for t in range(1, len(data))]
 
-    data_d1d12 = [data_d1[t] - data_d1[t - 7] for t in range(7, len(data_d1))]
-    scaler = MinMaxScaler()
-    data_d1d12_scaled = scaler.fit_transform(np.array(data_d1d12).reshape(-1, 1))
-    data_d1d12_scaled = [u[0] for u in data_d1d12_scaled]
-    P = 7
+        data_d1d12 = [data_d1[t] - data_d1[t - 7] for t in range(7, len(data_d1))]
+        scaler = MinMaxScaler()
+        data_d1d12_scaled = scaler.fit_transform(np.array(data_d1d12).reshape(-1, 1))
+        data_d1d12_scaled = [u[0] for u in data_d1d12_scaled]
+        P = 7
 
-    X = []
-    for t in range(P - 1, len(data_d1d12_scaled) - 1):
-        X.append([data_d1d12_scaled[t - n] for n in range(P)])
+        X = []
+        for t in range(P - 1, len(data_d1d12_scaled) - 1):
+            X.append([data_d1d12_scaled[t - n] for n in range(P)])
 
-    d = data_d1d12_scaled[P:]
-    mlp=pickle.load(open('src/models/precios-diarios.pkl', 'rb'))
+        d = data_d1d12_scaled[P:]
+        mlp=pickle.load(open('src/models/precios-diarios.pkl', 'rb'))
 
-    y_d1d12_scaled_m2 = mlp.predict(X)
-    
-    y_d1d12_scaled_m2 = data_d1d12_scaled[0:P] + y_d1d12_scaled_m2.tolist() #Agrega el periodo que se quito
+        y_d1d12_scaled_m2 = mlp.predict(X)
+        
+        y_d1d12_scaled_m2 = data_d1d12_scaled[0:P] + y_d1d12_scaled_m2.tolist() #Agrega el periodo que se quito
 
-    y_d1d12_m2 = scaler.inverse_transform([[u] for u in y_d1d12_scaled_m2]) #Escalado
-    y_d1d12_m2 = [u[0] for u in y_d1d12_m2.tolist()]                       
+        y_d1d12_m2 = scaler.inverse_transform([[u] for u in y_d1d12_scaled_m2]) #Escalado
+        y_d1d12_m2 = [u[0] for u in y_d1d12_m2.tolist()]                       
 
-    y_d1_m2 = [y_d1d12_m2[t] + data_d1[t] for t in range(len(y_d1d12_m2))] #Agregar componente ciclica
-    y_d1_m2 = data_d1[0:P] + y_d1_m2
+        y_d1_m2 = [y_d1d12_m2[t] + data_d1[t] for t in range(len(y_d1d12_m2))] #Agregar componente ciclica
+        y_d1_m2 = data_d1[0:P] + y_d1_m2
 
-    y_m2 = [y_d1_m2[t] + data[t] for t in range(len(y_d1_m2))]  #Agregar tendencia
+        y_m2 = [y_d1_m2[t] + data[t] for t in range(len(y_d1_m2))]  #Agregar tendencia
 
-    y_m2 = [data[0]] + y_m2
+        y_m2 = [data[0]] + y_m2
 
-    df.rename(columns = {'Precio':'Precio_Real'}, inplace = True)
+        df.rename(columns = {'Precio':'Precio_Real'}, inplace = True)
 
-    df['Precio_Pronostico']=y_m2
+        df['Precio_Pronostico']=y_m2
 
-    df.to_csv(path_forecasts,index=False)
+        df.to_csv(path_forecasts,index=False)
+
+    except: # pylint: disable=W0702
+        return None
 
 
 
--- src/models/train_daily_model.py
+++ src/models/train_daily_model.py
@@ -18,35 +18,39 @@ def train_daily_model():
     # raise NotImplementedError("Implementar esta función")
    
     #cwd = os.getcwd()
-    path = "data_lake/business/features/precios-diarios.csv"
-    path_model = "models/precios-diarios.pkl"
-    df = pd.read_csv(path)
-    data=list(df['Precio'])
+    try:
+        path = "data_lake/business/features/precios-diarios.csv"
+        path_model = "models/precios-diarios.pkl"
+        df = pd.read_csv(path)
+        data=list(df['Precio'])
 
-    data_d1 = [data[t] - data[t - 1] for t in range(1, len(data))]
-    data_d1d12 = [data_d1[t] - data_d1[t - 7] for t in range(7, len(data_d1))]
-    scaler = MinMaxScaler()
-    data_d1d12_scaled = scaler.fit_transform(np.array(data_d1d12).reshape(-1, 1))
-    data_d1d12_scaled = [u[0] for u in data_d1d12_scaled]
-    P = 7
-    X = []
-    for t in range(P - 1, len(data_d1d12_scaled) - 1):
-        X.append([data_d1d12_scaled[t - n] for n in range(P)])
-    d = data_d1d12_scaled[P:]
-    H = 5  # Se escoge arbitrariamente
+        data_d1 = [data[t] - data[t - 1] for t in range(1, len(data))]
+        data_d1d12 = [data_d1[t] - data_d1[t - 7] for t in range(7, len(data_d1))]
+        scaler = MinMaxScaler()
+        data_d1d12_scaled = scaler.fit_transform(np.array(data_d1d12).reshape(-1, 1))
+        data_d1d12_scaled = [u[0] for u in data_d1d12_scaled]
+        P = 7
+        X = []
+        for t in range(P - 1, len(data_d1d12_scaled) - 1):
+            X.append([data_d1d12_scaled[t - n] for n in range(P)])
+        d = data_d1d12_scaled[P:]
+        H = 5  # Se escoge arbitrariamente
 
-    np.random.seed(123456)
+        np.random.seed(123456)
 
-    mlp = MLPRegressor(
-        hidden_layer_sizes=(H,),
-        activation="tanh",
-        learning_rate="adaptive",
-        momentum=0.0,
-        learning_rate_init=0.002,
-        max_iter=100000,
-    )
-    mlp.fit(X[0:8675], data_d1d12_scaled[0:8675]) 
-    pickle.dump(mlp, open("src/models/precios-diarios.pkl", 'wb'))
+        mlp = MLPRegressor(
+            hidden_layer_sizes=(H,),
+            activation="tanh",
+            learning_rate="adaptive",
+            momentum=0.0,
+            learning_rate_init=0.002,
+            max_iter=100000,
+        )
+        mlp.fit(X[0:8675], data_d1d12_scaled[0:8675]) 
+        pickle.dump(mlp, open("src/models/precios-diarios.pkl", 'wb'))
+    
+    except: # pylint: disable=W0702
+        return None
 
 if __name__ == "__main__":
     import doctest

0f2a591341e8ff27bc3dc713edce84afbccc8ed8
Diego Mayorga <dcmayorgab@unal.edu.co>
2022-07-05 06:05:40 -0500

    Update make_features.py

--- src/features/make_features.py
+++ src/features/make_features.py
@@ -27,9 +27,12 @@ def make_features():
     #raise NotImplementedError("Implementar esta función")
     #cwd = os.getcwd()
     #path = os.path.join(cwd,"data_lake/business/precios-diarios.csv")
-    dfp = pd.read_csv("data_lake/business/precios-diarios.csv")
+    #dfp = pd.read_csv("data_lake/business/precios-diarios.csv")
     #dfp.to_csv(os.path.join(cwd, 'data_lake/business/features/precios_diarios.csv') , index=False )
-    dfp.to_csv('data_lake/business/features/precios-diarios.csv' , index=False )
+    #dfp.to_csv('data_lake/business/features/precios-diarios.csv' , index=False )
+
+    dfe = pd.read_csv('data_lake/business/precios-diarios.csv')
+    dfe.to_csv('data_lake/business/features/precios-diarios.csv', index=False)
 
 
 

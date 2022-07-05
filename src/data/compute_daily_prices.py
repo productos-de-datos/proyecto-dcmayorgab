d011d15440e727008a4838b8127f3c55cecb75d1
Diego Mayorga <dcmayorgab@unal.edu.co>
2022-07-05 09:22:35 -0500

    Documentacion

--- src/data/compute_daily_prices.py
+++ src/data/compute_daily_prices.py
@@ -1,22 +1,19 @@
-"""Compute los precios promedios diarios.
-
+"""
+Compute los precios promedios diarios.
 Usando el archivo data_lake/cleansed/precios-horarios.csv, compute el prcio
 promedio diario (sobre las 24 horas del dia) para cada uno de los dias. Las
 columnas del archivo data_lake/business/precios-diarios.csv son:
 
 * fecha: fecha en formato YYYY-MM-DD
-
 * precio: precio promedio diario de la electricidad en la bolsa nacional
 
 """
-
 
 import pandas as pd
 
 def compute_daily_prices():
     """
     Computa los precios promedios diarios.
-
     """
     #raise NotImplementedError("Implementar esta función")
 
